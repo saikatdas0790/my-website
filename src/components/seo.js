51c30a1446ef0f61f4db4c62313d338e93117580
Saikat Das <saikatdas0790@gmail.com>
2020-06-09 18:53:36 +0530

    chore: update outdated packages

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 
 function SEO({ description, lang, meta, title, pageName, url, image }) {
