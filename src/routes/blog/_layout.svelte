9b4f909c9473a6d965d8f397da2df7efca5fd2fc
Saikat Das <saikatdas0790@gmail.com>
2021-06-26 22:21:10 +0530

    feat: add individual blog post layout

---
+++ src/routes/blog/_layout.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { page } from "$app/stores";
+
+  export let title;
+  export let description;
+  export let author;
+  export let tags;
+  export let date;
+  export let icon;
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description} />
+  <meta name="keywords" content={tags.join(", ")} />
+  <meta name="author" content={author} />
+
+  <!-- Twitter Card data -->
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@saikatdas0790" />
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={description} />
+  <meta
+    name="twitter:image"
+    content={`${$page.host}/assets/icons/post-topics/${icon}`} />
+
+  <!-- Open Graph data -->
+  <meta property="og:url" content={`${$page.host}${$page.path}`} />
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+  <meta
+    property="og:image"
+    content={`${$page.host}/assets/icons/post-topics/${icon}`} />
+</svelte:head>
+
+<article class="prose prose-emerald max-w-screen-md mx-auto my-8 px-4">
+  <h1 class="!mb-2">{title}</h1>
+  <span>
+    published on <span class="text-emerald-700 italic font-medium">
+      {new Date(date).toDateString()}
+    </span>
+  </span>
+
+  <slot />
+  <!-- 
+  <p class="text-emerald-600 text-xl !my-14">
+    {tags.map((tag) => `#${tag}`).join(" ")}
+  </p> -->
+</article>
