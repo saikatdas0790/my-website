af55116eacaf5f0cf62d51ff23e2276694d613ed
Saikat Das <saikatdas0790@gmail.com>
2021-06-27 22:18:57 +0530

    feat: add error fallback page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,20 @@
+<main class="max-w-screen-md mx-auto p-2">
+  <img
+    src="/assets/images/pages/__error/pupper.jpg"
+    alt="Sorry looking pupper"
+    class="w-full" />
+  <p class="text-xl">
+    Hey there, time traveller! You seem to have visited this page in the past
+    when it doesn't exist yet!
+  </p>
+  <p>
+    Why not visit again in the future when this is available? In the meantime,
+    why not <a
+      sveltekit:prefetch
+      href="/blog"
+      class="text-fuchsia-500 font-semibold">
+      check out
+    </a>
+    some of our recent posts on full stack Javascript
+  </p>
+</main>
